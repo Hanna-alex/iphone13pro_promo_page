7691f3bbd2570dffef79a2547b278bfeb4663ea3
Hanna-alex <64468077+Hanna-alex@users.noreply.github.com>
2022-01-24 01:59:13 +0600

    Add files via upload

--- js/accordion.js
+++ js/accordion.js
@@ -2,10 +2,10 @@ const accordion = () => {
   const chItems = document.querySelectorAll('.characteristics__item');
 
   const tabsClose = () => {
-    chItems.forEach((el) => {
-        el.querySelector('.characteristics__title').classList.remove('active');
-        el.querySelector('.characteristics__description').classList.remove('open');
-        el.querySelector('.characteristics__description').style.height = '';
+    chItems.forEach((item) => {
+      item.querySelector('.characteristics__title').classList.remove('active');
+      item.querySelector('.characteristics__description').classList.remove('open');
+      item.querySelector('.characteristics__description').style.height = '';
     });
   };
   
@@ -14,7 +14,6 @@ const accordion = () => {
   chItems.forEach(item => {
     const chButton = item.querySelector('.characteristics__title');
     const chContent = item.querySelector('.characteristics__description');
-    console.log(chButton)
    
     chButton.addEventListener('click', () => {      
      
