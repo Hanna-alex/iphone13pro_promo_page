d46f013617e7a49474d033d0ca6dd68d678fb733
Hanna-alex <64468077+Hanna-alex@users.noreply.github.com>
2022-01-25 03:41:22 +0600

    create get and send form

--- js/getData.js
+++ js/getData.js
@@ -1,5 +1,58 @@
 const getData = () => {
-  fetch('https://jsonplaceholder.typicode.com/todos')
+  const list = document.querySelector('.cross-sell__list');
+  const btn = document.querySelector('.cross-sell__add');
+
+  let stack = 4;
+  let count = 1;
+
+  //console.log(list);
+    
+  const render = (data) => {   
+    list.innerHTML = '';
+    data.forEach(item => {
+      //console.log(item)
+      // 
+      list.insertAdjacentHTML('beforeend', `
+      <li>
+              <article class="cross-sell__item">
+                <img
+                  class="cross-sell__image"
+                  src="./${item.photo}"
+                  alt="${item.id}"
+                />
+                <h3 class="cross-sell__title">
+                  ${item.name}
+                </h3>
+                <p class="cross-sell__price">${item.price}₽</p>
+                <button
+                  type="button"
+                  class="button button_buy cross-sell__button"
+                >
+                  Купить
+                </button>
+              </article>
+            </li>`)
+    })
+  }
+
+  const sliceArray = (data, index) => {
+    return data.slice(0, index)
+  }
+
+  const changeData = (data) => {
+    const newStack = stack * count
+    render(sliceArray(data, newStack))
+
+    if (data.length > newStack) {
+      count++
+    } else {
+      //убирать кнопку
+      btn.style.display = 'none'
+    }
+  }
+
+  const getGoods = () => {
+    fetch('/cross-sell-dbase/dbase.json')
     .then((response) => {
       if (response.ok) {
         return response.json()
@@ -7,15 +60,19 @@ const getData = () => {
         throw new Error('Данные получены с ошибкой')
       }
     })
+
     .then((data) => {
-      console.log(data);
+      changeData(data);
     })
+
     .catch((error) => {
       console.error(error.message);
     })
-    .finally(() => {
-    console.log('finally')
-  })
+  }
+  btn.addEventListener('click', getGoods)
+  
+  getGoods()
+    
   
 }
 
