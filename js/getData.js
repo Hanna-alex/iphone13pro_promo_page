ecedc4576f7eb938b48d128670637ab47520e8a6
Hanna-alex <64468077+Hanna-alex@users.noreply.github.com>
2022-03-14 18:53:25 +0600

    Update getData.js

--- js/getData.js
+++ js/getData.js
@@ -52,7 +52,7 @@ const getData = () => {
   }
 
   const getGoods = () => {
-    fetch('../cross-sell-dbase/dbase.json')
+    fetch('./cross-sell-dbase/dbase.json')
     .then((response) => {
       if (response.ok) {
         return response.json()
